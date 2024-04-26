3b93d3706054245871b948e7b724fa627b15a66e
blake1821 <124000747+blake1821@users.noreply.github.com>
2024-04-25 19:42:30 -0500

    Add files via upload

---
+++ connect.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+VPN_HOME=~
+cd $VPN_HOME
+
+# get variables from setup.sh
+cd vars
+LAN_IFNAME=$(cat lan)
+WAN_IFNAME=$(cat wan)
+MAN_IFNAME=$(cat man)
+DNS_SERVER=$(cat dns)
+DNS6_SERVER=$(cat dns6)
+MY_IPV4_ADDRESS=$(cat my4)
+MY_IPV6_ADDRESS=$(cat my6)
+cd ..
+PRIVATE_KEY_FILE=privatekey
+PUBLIC_KEY_FILE=publickey
+
+# constants
+WG_IFNAME=wg0
+LOCATION=us
+PORT=51820
+
+# Get relays
+if ! [ -f relays ]; then
+    curl https://api.mullvad.net/app/v1/relays > relays
+
+    RELAY_IPS=$(mktemp);
+    RELAY_PUBKEYS=$(mktemp);
+
+    cat relays  \
+        | jq -r ".wireguard.relays[] | select(.location | startswith(\"$LOCATION\")) | .ipv4_addr_in" \
+        >$RELAY_IPS
+    cat relays  \
+        | jq -r ".wireguard.relays[] | select(.location | startswith(\"$LOCATION\")) | .public_key" \
+        >$RELAY_PUBKEYS
+
+    paste $RELAY_IPS $RELAY_PUBKEYS >relay_ip_pubkeys
+
+    rm $RELAY_IPS
+    rm $RELAY_PUBKEYS
+fi
+
+# Select a random IP
+ENDPOINT_IP_PUBKEY=`shuf relay_ip_pubkeys | head -1`
+ENDPOINT_IP=`echo "$ENDPOINT_IP_PUBKEY" | cut -f1`
+ENDPOINT_PUBKEY=`echo "$ENDPOINT_IP_PUBKEY" | cut -f2`
+
+# Set up the interface
+ip link del $WG_IFNAME 2>/dev/null
+ip link add dev $WG_IFNAME type wireguard
+ip addr add dev $WG_IFNAME $MY_IPV4_ADDRESS
+ip -6 addr add dev $WG_IFNAME $MY_IPV6_ADDRESS
+wg set $WG_IFNAME \
+    listen-port $PORT \
+    private-key $PRIVATE_KEY_FILE \
+    peer $ENDPOINT_PUBKEY \
+        endpoint $ENDPOINT_IP:$PORT \
+        allowed-ips 0.0.0.0/0,::/0
+ip link set up dev $WG_IFNAME
+
+# Set up routes
+DEFAULT_GATEWAY=$(ip route show | grep "default via" | awk '{print $3}')
+ip route add $ENDPOINT_IP via $DEFAULT_GATEWAY
+ip route add 0.0.0.0/1 dev $WG_IFNAME
+ip route add 128.0.0.0/1 dev $WG_IFNAME
+ip -6 route delete default
+ip -6 route add default dev $WG_IFNAME
+
+# Enable forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
+# Set up firewall rules
+    # Flush existing rules and set default chain policies for IPv4
+    iptables -F
+    iptables -X
+    iptables -P INPUT DROP
+    iptables -P FORWARD DROP
+    iptables -P OUTPUT ACCEPT
+
+    # Flush existing rules and set default chain policies for IPv6
+    ip6tables -F
+    ip6tables -X
+    ip6tables -P INPUT DROP
+    ip6tables -P FORWARD DROP
+    ip6tables -P OUTPUT ACCEPT
+
+    # Allow loopback interface to function properly for IPv4 and IPv6
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -A OUTPUT -o lo -j ACCEPT
+    ip6tables -A INPUT -i lo -j ACCEPT
+    ip6tables -A OUTPUT -o lo -j ACCEPT
+
+    # Allow traffic to WAN
+    iptables -A INPUT -i $WAN_IFNAME -j ACCEPT
+
+    # Forward traffic from $LAN_IFNAME to $WG_IFNAME for IPv4 and IPv6
+    iptables -A FORWARD -i $LAN_IFNAME -o $WG_IFNAME -j ACCEPT
+    ip6tables -A FORWARD -i $LAN_IFNAME -o $WG_IFNAME -j ACCEPT
+    iptables -A FORWARD -i $WG_IFNAME -m state --state ESTABLISHED,RELATED -j ACCEPT
+    ip6tables -A FORWARD -i $WG_IFNAME -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+    # Block all incoming connections on $LAN_IFNAME except for DHCP for IPv4 and IPv6
+    iptables -A INPUT -i $LAN_IFNAME -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+    ip6tables -A INPUT -i $LAN_IFNAME -p udp --dport 546:547 --sport 546:547 -j ACCEPT
+
+    # Allow all incoming connections on $MAN_IFNAME for IPv4 and IPv6
+    iptables -A INPUT -i $MAN_IFNAME -j ACCEPT
+    ip6tables -A INPUT -i $MAN_IFNAME -j ACCEPT
+
+    # Allow established connections to input
+    iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+    ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+    iptables -t nat -A POSTROUTING -o $WG_IFNAME -j MASQUERADE
+    ip6tables -t nat -A POSTROUTING -o $WG_IFNAME -j MASQUERADE
+
+# Setup DNS
+cat >/etc/resolv.conf <<EOF
+nameserver $DNS_SERVER
+nameserver $DNS6_SERVER
+EOF
+
+# Restart dante
+systemctl restart danted
---
+++ setup.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+VPN_HOME=~
+cd $VPN_HOME
+
+echo VPN Router Setup
+echo ==============================
+echo
+
+# Install packages
+apt install wireguard jq curl iptables isc-dhcp-server dante-server
+
+# Prompt for information
+echo Here are the different interfaces: 
+ip link show | awk -F': ' '/^[0-9]/ {print $2}'
+echo
+read -p "Enter the WAN interface name: " WAN_IFNAME
+read -p "Enter the MAN interface name: " MAN_IFNAME
+read -p "Enter the LAN interface name: " LAN_IFNAME
+echo
+echo Go to mullvad.net and create a new device.
+echo Generating Private Key...
+PRIVATE_KEY=$(wg genkey)
+PUBLIC_KEY=$(echo $PRIVATE_KEY | wg pubkey)
+echo The public key is $PUBLIC_KEY
+read -p "Enter the IPV4 From Mullvad: " MY_IPV4_ADDRESS
+read -p "Enter the IPV6 From Mullvad: " MY_IPV6_ADDRESS
+
+
+DNS_SERVER=1.1.1.1
+DNS6_SERVER=2606:4700:4700::1111
+
+# Write Variables
+echo $PRIVATE_KEY >privatekey
+echo $PUBLICK_KEY >publickey
+cd vars
+echo $LAN_IFNAME>lan
+echo $WAN_IFNAME>wan
+echo $MAN_IFNAME>man
+echo $DNS_SERVER>dns
+echo $DNS6_SERVER>dns6
+echo $MY_IPV4_ADDRESS>my4
+echo $MY_IPV6_ADDRESS>my6
+cd ..
+
+# /etc/network/interfaces
+cat >/etc/network/interfaces <<EOF
+source /etc/network/interfaces.d/*
+
+auto lo
+iface lo inet loopback
+
+auto $WAN_IFNAME
+allow-hotplug $WAN_IFNAME
+iface $WAN_IFNAME inet dhcp
+
+auto $MAN_IFNAME
+allow-hotplug $MAN_IFNAME
+iface $MAN_IFNAME inet dhcp
+
+auto $LAN_IFNAME
+iface $LAN_IFNAME inet static
+        address 192.168.0.1/24
+iface $LAN_IFNAME inet6 static
+        address 2001:db8::1/64
+EOF
+
+# /etc/dhcp
+# dhcpd.conf
+cat >/etc/dhcpd/dhcpd.conf <<EOF
+option domain-name-servers $DNS_SERVER;
+
+default-lease-time 600;
+max-lease-time 7200;
+ddns-update-style none;
+
+subnet 192.168.0.0 netmask 255.255.255.0 {
+    range 192.168.0.2 192.168.0.254;
+    option routers 192.168.0.1;
+
+    interface $LAN_IFNAME;
+}
+EOF
+
+# dhcpd6.conf
+cat >/etc/dhcpd/dhcpd6.conf <<EOF
+
+default-lease-time 2592000;
+preferred-lifetime 604800;
+option dhcp-renewal-time 3600;
+option dhcp-rebinding-time 7200;
+allow leasequery;
+
+option dhcp6.name-servers $DNS6_SERVER;
+
+subnet6 2001:db8::/64 {
+    range6 2001:db8::100 2001:db8::1:0;
+    interface $LAN_IFNAME;
+}
+
+EOF
+
+# /etc/danted.conf
+cat >/etc/danted.conf <<EOF
+logoutput: syslog
+user.privileged: root
+user.unprivileged: nobody
+
+# The listening network interface or address.
+internal: 0.0.0.0 port=1080
+
+# The proxying network interface or address.
+external: $WG_IFNAME
+
+# socks-rules determine what is proxied through the external interface.
+clientmethod: none
+socksmethod: none
+
+client pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+}
+
+socks pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+}
+EOF
